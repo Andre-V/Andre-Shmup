60695c284b68b400a9ccde502c1fc225cd1e9d05
Andre Villalon <29933382+Andre-V@users.noreply.github.com>
2021-11-16 23:21:05 +1100

    removed unused priority property

--- System.h
+++ System.h
@@ -7,15 +7,5 @@ class System
 public:
 	EntityManager* entitySource;
 	GameInfo* gameInfo;
-	int priority;
 	virtual void update() { }
 };
-
-struct SystemCompare
-{
-	bool operator()(const unique_ptr<System>& lhs, const unique_ptr<System>& rhs) const
-	{
-		return lhs->priority < rhs->priority;
-	}
-};
-
